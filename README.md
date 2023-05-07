5dcec4394f82cefc744222e745d1a57189e95cca
xNykram <nykram@wp.pl>
2023-05-07 14:59:53 +0200

    - update readme, save plt locally

--- README.md
+++ README.md
@@ -8,6 +8,14 @@
 
 **Gender Recognition** - is a small project that classifies gender based on the avatars of the gods from the Smite game using PyTorch.
 
-### Example prediction
+### Example predictions:
+<details>
+<summary>Prediction 1</summary>
 
-![test](https://i.postimg.cc/SRt9cS1p/demo.png)
+![pred_1](https://i.postimg.cc/KzQN1ynn/demo3.png)
+</details>
+<details>
+<summary>Prediction 2</summary>
+
+![pred_2](https://i.postimg.cc/3xWP7tQS/demo2.png)
+</details>
--- src/predict.py
+++ src/predict.py
@@ -1,6 +1,8 @@
 from src.utils.images import load_random_predict_image
 from src.train import train_model
 import torch
+import matplotlib.pyplot as plt   
+import numpy as np
 
 model = train_model()
 
@@ -12,6 +14,12 @@ with torch.no_grad():
     _, predicted = torch.max(output.data, 1)
 
 if predicted.item() == 1:
+    gender = 'woman'
     print('The model predicts that the person in the image is a woman.')
 else:
+    gender = 'man'
     print('The model predicts that the person in the image is a man.')
+
+plt.imshow(random_predict_image.squeeze().numpy(), cmap='gray')
+plt.title(f'Prediction: {gender}')
+plt.savefig('src/output/output.png')
